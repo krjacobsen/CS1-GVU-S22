480ad453b1a352109c21347caac05a4b9cae4e7e
MRobbins-GVU <mrobbins@grandview.edu>
2022-02-03 19:27:34 -0600

    updated code from class

--- Feb-3/evens.py
+++ Feb-3/evens.py
@@ -3,6 +3,17 @@
 def evens(items):
     """ Return list of even values in items. """
     result = []
+    for item in data:
+        if item % 2 == 0:
+            # result += list(item)
+            result += [item]
+    
+    return result
     
 def main():
-    data = [93, 32, 28, 2, 56, 99, 31, 5, 65, 39, 83, 42, 24, 40, 52, 38]
+    data = [93, 32, 28, 2, 56, 99, 17, 14, 3]
+    even_data = evens(data)
+    
+    print(even_data)
+
+main()
