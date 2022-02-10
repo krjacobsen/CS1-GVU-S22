6ae6eda697de3723aa21680cdd498f39d2f9bf1c
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-02-09 21:05:35 -0600

    Create shell of question 2

---
+++ Exams/Exam1/question2.py
@@ -0,0 +1,19 @@
+# add evens
+
+def add_evens(start, stop):
+    # add up all even numbers between start and stop, not including either.
+    total = 0
+    
+    # TODO: You will need to define a variable and decide what goes in the range function, and how to add up the numbers
+    for  in range():
+        
+    return total
+
+def main():
+    start_1 = 0
+    stop_1 = 10
+    ex_1 = add_evens(start_1, stop_1)
+    # TODO: You will need to modify this print statement
+    print("All evens between start and stop add up to value")
+    
+    # TODO: Test the second value pair of 3, 9
