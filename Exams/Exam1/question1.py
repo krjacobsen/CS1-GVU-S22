b86ce29f1c332fb5282ff09c3e3c77095b090309
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-02-09 21:01:44 -0600

    Add shell of Exam1 q1

---
+++ Exams/Exam1/question1.py
@@ -0,0 +1,17 @@
+
+# Question 1
+# Reviewing the polygon() function may help
+
+from turtle import *
+
+def polygon_at(x, y, num_sides):
+    # Draw a polygon with num_sides at x, y
+    
+def main():
+    polygon_at(0, 0, 6)
+    polygon_at(50,50,6)
+    polygon_at(100,100,3)
+    
+    exitonclick()
+    
+main()
